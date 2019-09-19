ed031342db5a5b87db1fa89b2a308863e96f3431
Jonathan Daugherty <cygnus@foobox.com>
2019-09-19 12:41:48 -0700

    Draw.LeaveChannelConfirm: if leaving a channel entails closing conversations, mention those in the confirmation dialog

--- src/Draw/LeaveChannelConfirm.hs
+++ src/Draw/LeaveChannelConfirm.hs
@@ -10,6 +10,8 @@ import Prelude.MH
 import Brick
 import Brick.Widgets.Border
 import Brick.Widgets.Center
+import Data.List ( sortBy )
+import Data.Ord ( comparing )
 
 import Draw.Main
 import Themes
@@ -23,11 +25,28 @@ drawLeaveChannelConfirm st =
 confirmBox :: ChatState -> Widget Name
 confirmBox st =
     let cName = st^.csCurrentChannel.ccInfo.cdName
-    in centerLayer $ hLimit 50 $ vLimit 15 $
+        isConvoFor cId (otherCr, _) = case otherCr of
+            ConversationChannel otherCid _ -> otherCid == cId
+            _ -> False
+        cr = st^.csCurrentChannelRef
+        convs = case cr of
+            ServerChannel thisCid ->
+                sortBy (comparing (_cdName . _ccInfo . snd)) $
+                filteredChannels (isConvoFor thisCid) (st^.csChannels)
+            _ -> []
+        ppConv (_, cc) = hCenter $ withDefAttr dialogEmphAttr $ txt $ cc^.ccInfo.cdName
+    in centerLayer $ hLimit 50 $
        withDefAttr dialogAttr $
        borderWithLabel (txt "Confirm Leave Channel") $
        vBox [ padBottom (Pad 1) $ hCenter $ txt "Are you sure you want to leave this channel?"
             , padBottom (Pad 1) $ hCenter $ withDefAttr dialogEmphAttr $ txt cName
+            , if null convs
+              then emptyWidget
+              else padBottom (Pad 1) $
+                   vBox [ hCenter $ txt "Leaving this channel will also close the "
+                        , padBottom (Pad 1) $ hCenter $ txt "following conversations that you are following:"
+                        , vBox $ ppConv <$> convs
+                        ]
             , hCenter $ txt "Press " <+> (withDefAttr dialogEmphAttr $ txt "Y") <+> txt " to leave the channel"
             , hCenter $ txt "or any other key to cancel."
             ]
